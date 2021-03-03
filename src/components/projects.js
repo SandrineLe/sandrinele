040399490c87142c3a31dcd83e6b5fb77ba5d8a6
SandrineLe <le.sandrinemt@gmail.com>
2021-03-03 15:38:41 +0100

    removing fourmis

--- src/components/projects.js
+++ src/components/projects.js
@@ -13,7 +13,6 @@ import risingsun from ".././img/project-risingsun.png"
 import pomodoro from ".././img/project-pomodoro.png"
 import goutte from ".././img/project-goutte.png"
 import breeze from ".././img/project-breeze.png"
-import fourmis from ".././img/project-fourmis.png"
 import dissidentes from ".././img/project-dissidentes.png"
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -62,15 +61,6 @@ const portfolio = [
         website: "#",
         github: "https://github.com/macmowl/mwenbwa",
         category: ["all", "website", "others"]
-    },
-    {
-        name: "Les Fourmis, le collectif",
-        photo: fourmis,
-        desc: "(In progress) Personal project, creation of a website (design and coding) for a fictional collective : Les Fourmis. ",
-        tools: "HTML - CSS - BOOTSTRAP",
-        website: "https://sandrinele.github.io/lesfourmis/",
-        github: "https://github.com/SandrineLe/lesfourmis",
-        category: ["all", "website"]
     },
     {
         name: "Interior design agency",
